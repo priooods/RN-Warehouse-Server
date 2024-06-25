8ee5cac5ac73fc881581425e2aed1650ad96b71b
priooods <priodwisembodo23@gmail.com>
2024-06-25 16:18:05 +0700

    Configuration

--- Modules/Manage/app/Http/Controllers/TransaksiController.php
+++ Modules/Manage/app/Http/Controllers/TransaksiController.php
@@ -6,9 +6,24 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use Modules\Manage\Models\MTabungTab;
+use Modules\Manage\Models\TTabungTab;
 
 class TransaksiController extends Controller
 {
+    protected $controller;
+    protected $mTabungTab;
+    protected $tTabungTab;
+    public function __construct(
+        Controller $controller, 
+        MTabungTab $mTabungTab,
+        TTabungTab $tTabungTab
+    ) {
+        $this->controller = $controller;
+        $this->mTabungTab = $mTabungTab;
+        $this->tTabungTab = $tTabungTab;
+    }
     /**
      * Display a listing of the resource.
      */
@@ -28,9 +43,47 @@ class TransaksiController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request)
     {
-        //
+        $this->controller->validasi($request, [
+            'condition' => 'required',
+            'type_transaction' => 'required',
+            'no_po' => 'required',
+            'surat_jalan' => 'required',
+            'm_type_tabs_id' => 'required',
+            'sender' => 'required',
+            'receiver' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            foreach ($request->tabung as $value) { // looping input tabung yang banyak itu
+                $findTabung = $this->mTabungTab->where('code', $value->code)->first(); // cari udah exist belum ?
+                if(!isset($findTabung)) { // tambah ke master code kalau belum exist
+                    $tabung = $this->mTabungTab->create([
+                        'code' => $value->code
+                    ]);
+                }
+                // tambahkan transaksi tabung
+                $this->tTabungTab->create([
+                    'm_tabung_tabs_id' => $tabung->id,
+                    'type_transaction' => $tabung->type_transaction,
+                    'condition' => $tabung->condition,
+                    'no_po' => $tabung->no_po,
+                    'surat_jalan' => $tabung->surat_jalan,
+                    'm_type_tabs_id' => $tabung->m_type_tabs_id,
+                    'sender' => $tabung->sender,
+                    'receiver' => $tabung->receiver,
+                    'm_user_access_tabs_id' => auth()->user()->id,
+                ]);
+            }
+            DB::commit();
+            return $this->controller->success('TRANSAKSI ADD', null);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(500, $th->getMessage());
+        }
     }
 
     /**
@@ -52,9 +105,17 @@ class TransaksiController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id): RedirectResponse
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $this->tTabungTab->where('id', $id)->update($request->all());
+            DB::commit();
+            return $this->controller->success('TRANSAKSI UPDATE', null);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(500, $th->getMessage());
+        }
     }
 
     /**
@@ -62,6 +123,14 @@ class TransaksiController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $this->tTabungTab->where('id', $id)->delete();
+            DB::commit();
+            return $this->controller->success('TRANSAKSI DELETE', null);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            abort(500, $th->getMessage());
+        }
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 abstract class Controller
 {
-    //
+    public function validasi(Request $request, $rules, $message = null){
+        $validasi = Validator::make($request->all(), $rules, $message);
+        if($validasi->fails()) { abort(402, $validasi->errors()->all()); }
+        return null;
+    }
+
+    public function success($msg, $data, $notif = null, $code = 200){
+        return response()->json(
+            [
+                'res_message' => $msg,
+                'res_data' => $data,
+                'notif' => $notif,
+            ], $code
+        );
+    }
 }
